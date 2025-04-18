c50702dcb7a430907c2d8c5ac8d8e666a265fa61
ThulasiMS94 <shadeepsa3@gmail.com>
2025-04-18 20:44:20 +0530

    Add files via upload

---
+++ marathon/TicketBooking.java
@@ -0,0 +1,33 @@
+package marathon;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TicketBooking {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.pvrcinemas.com");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("(//span[@class='cities-placed'])[2]")).click();
+		driver.findElement(By.xpath("//span[@class='sc-bdfCDU gCCOIc']")).click();
+		driver.findElement(By.xpath("//span[@class='cinemas-inactive']")).click();
+		driver.findElement(By.xpath("//span[text()='Select Cinema']")).click();
+		driver.findElement(By.xpath("//span[text()='INOX National,Virugambakkam Chennai']")).click();
+		//driver.findElement(By.xpath("//span[text()='Select Date']")).click();
+		driver.findElement(By.xpath("//li//span[text()='Tomorrow']")).click();
+		//driver.findElement(By.xpath("//span[text()='Select Movie']")).click();
+		driver.findElement(By.xpath("//li//span[text()='GOOD BAD UGLY']")).click();
+		//driver.findElement(By.xpath("//span[text()='Select Timing']")).click();
+		driver.findElement(By.xpath("//span[text()='12:25 PM']")).click();
+		driver.findElement(By.xpath("(//span[text()='Book'])[1]")).click();
+		driver.findElement(By.xpath("//button[text()='Accept']")).click();
+		driver.findElement(By.id("SL.SILVER|F:12")).click();
+		driver.findElement(By.xpath("//button[text()='Proceed']")).click();
+		}
+
+}
